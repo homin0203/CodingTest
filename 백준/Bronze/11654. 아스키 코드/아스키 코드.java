eaf9fbebffd935c3d6fff263cd9be009e661ba19
homin0203 <116356234+homin0203@users.noreply.github.com>
2023-04-27 00:55:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        
+        //출력
+        System.out.println((int)ch);
+    }
+}
